96f8fc105cc59fb4cbe8e15bd1c8989161080482
aruna003 <108563112+aruna003@users.noreply.github.com>
2024-01-30 23:48:40 +0530

    Add files via upload

---
+++ longest_substr.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        l=[]
+        sub_s=""
+        for i in s:
+            print("i: ",i,"subs: ",sub_s)
+            if i not in sub_s:
+                sub_s+=i
+            else:
+                # print(sub_s)
+                l.append(len(sub_s))
+                index=sub_s.find(i)
+                sub_s=sub_s[index+1:]
+                sub_s+=i
+        l.append(len(sub_s))
+
+        return max(l)
+    
+if __name__=="__main__":
+    sln=Solution()
+    n=sln.lengthOfLongestSubstring("aab")
+    print(n)
---
+++ zigzag.py
@@ -0,0 +1,21 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+
+        n=(numRows-1)+(numRows-2)+1
+        numRows%n
+        l=len(s)
+        levels=[]
+        for i in range(n):
+            sub_level=[]
+            c=0
+            while c<=l:
+                sub_level.append(s[i+c])
+                c+=c
+            levels.append(sub_level)
+        print(levels)
+            
+if __name__=="__main__":
+    sln=Solution()
+    n=sln.convert("PAYPALISHIRING",3)
+    print("hello")
+    # print(n)
