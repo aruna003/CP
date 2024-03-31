fcb68732ab8d306e53909b553de54ee41189f874
aruna003 <108563112+aruna003@users.noreply.github.com>
2024-03-31 12:24:35 +0530

    Create LetterCmbnOfAPhNo.py

---
+++ LetterCmbnOfAPhNo.py
@@ -0,0 +1,16 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        d={'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        res=[]
+        if digits=="":
+            return []
+        def backtrack(combination, next_digits):
+            if not next_digits:
+                res.append(combination)
+                return
+            
+            for letter in d[next_digits[0]]:
+                backtrack(combination + letter, next_digits[1:])
+        
+        backtrack("", digits)
+        return res
